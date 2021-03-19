0363a296603fca2ade64b2a430dc3c62f8472486
Jaden Padua <paduajaden@gmail.com>
2021-03-18 17:52:08 -0700

    Boilerplating

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.less';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Product.ly</h1>
+        </div>
+    );
+};
+
+export default App;
